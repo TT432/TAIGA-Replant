bd8db7ee7df3b78cc505d8052f6e0140b1efad4e
Giovanni Harting <539@idlegandalf.com>
2016-05-02 12:42:53 +0200

    added own WorldGenMinable to search for nearby blocks instead ob replacing them

--- src/main/java/main/util/Generator.java
+++ src/main/java/main/util/Generator.java
@@ -5,7 +5,6 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.gen.feature.WorldGenMinable;
 
 import java.util.Random;
 
@@ -35,7 +34,7 @@ public class Generator {
             int posX = chunkX + random.nextInt(16);
             int posY = random.nextInt(height) + minY;
             int posZ = chunkZ + random.nextInt(16);
-            new WorldGenMinable(state, size, StateMatcher.forState(replace, property, comparable)).generate(world, random, new BlockPos(posX, posY, posZ));
+            new ZWorldGenMinable(state, size, StateMatcher.forState(replace, property, comparable)).generate(world, random, new BlockPos(posX, posY, posZ));
         }
     }
 }
--- src/main/java/main/util/StateMatcher.java
+++ src/main/java/main/util/StateMatcher.java
@@ -3,6 +3,8 @@ package main.util;
 import com.google.common.base.Predicate;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 
 public class StateMatcher implements Predicate<IBlockState> {
@@ -20,13 +22,57 @@ public class StateMatcher implements Predicate<IBlockState> {
         return new StateMatcher(state, property, value);
     }
 
-    public boolean apply(IBlockState state) {
+    public boolean apply(IBlockState state, BlockPos pos, World world) {
         if (state != null) {
-            if (property != null && value != null)
-                return state.getBlock() == this.state.getBlock() && state.getValue(property) == value;
-            else
+            if (property != null && value != null) {
+                if (checkLayerForBlocks(pos.getX(), pos.getZ(), pos.getY() - 1, world, pos) ||
+                        checkLayerForBlocks(pos.getX(), pos.getZ(), pos.getY(), world, pos) ||
+                        checkLayerForBlocks(pos.getX(), pos.getZ(), pos.getY() + 1, world, pos))
+                    return true;
+
+            } else
                 return state.getBlock() == this.state.getBlock();
         }
+        return false;
+    }
+
+    @Override
+    public boolean apply(IBlockState input) {
+        if (state != null) {
+            if (property != null && value != null) {
+                return state.getBlock() == this.state.getBlock() && state.getValue(property) == value;
+            } else
+                return state.getBlock() == this.state.getBlock();
+        }
+        return false;
+    }
+
+    private boolean checkLayerForBlocks(int X, int Z, int Y, World world, BlockPos origin) {
+        int x = 0, z = 0, dx = 0, dz = -1;
+        int t = Math.max(X, Z);
+        int maxI = t * t;
+
+        for (int i = 0; i < maxI; i++) {
+            if ((-X / 2 <= x) && (x <= X / 2) && (-Z / 2 <= z) && (z <= Z / 2)) {
+                System.out.println(x + "," + z);
+                if (new BlockPos(x, Y, z) == origin)
+                    continue;
+
+                IBlockState bState = world.getBlockState(new BlockPos(x, Y, z));
+                if (bState.getBlock() == this.state.getBlock() && bState.getValue(property) == value) {
+                    return true;
+                }
+            }
+
+            if ((x == z) || ((x < 0) && (x == -z)) || ((x > 0) && (x == 1 - z))) {
+                t = dx;
+                dx = -dz;
+                dz = t;
+            }
+            x += dx;
+            z += dz;
+        }
+
         return false;
     }
 }
