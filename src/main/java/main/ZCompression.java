ec996ab1af803e299b7e7076818380b57d5ba9c0
Giovanni Harting <539@idlegandalf.com>
2016-05-02 18:02:10 +0200

    fixed derp in registerBlocks

--- src/main/java/main/ZCompression.java
+++ src/main/java/main/ZCompression.java
@@ -1,30 +1,10 @@
 package main;
 
-import blocks.BlockAardiumOre;
-import blocks.BlockAdamantiteOre;
-import blocks.BlockArcaniteOre;
-import blocks.BlockAxiidianOre;
-import blocks.BlockBasalt;
-import blocks.BlockBismuthOre;
-import blocks.BlockEterniteOre;
-import blocks.BlockIgnititeOre;
-import blocks.BlockKarmesineOre;
-import blocks.BlockLigniteOre;
-import blocks.BlockMeteoriteOre;
-import blocks.BlockMindoriteOre;
-import blocks.BlockMythrilOre;
-import blocks.BlockPalladiumOre;
-import blocks.BlockPrometheumOre;
-import blocks.BlockRottenGround;
-import blocks.BlockSlaggoldOre;
-import blocks.BlockSlagironOre;
-import blocks.BlockTiberiumOre;
-import blocks.BlockVibraniumre;
-import blocks.BlockYrdeanOre;
-import blocks.category.BasicBlockOre;
+import blocks.*;
 import blocks.category.BasicBlockOreGlow;
 import items.ItemSlagiron;
 import main.world.ZWorldGen;
+import net.minecraft.block.Block;
 import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -40,14 +20,8 @@ public class ZCompression {
 
     static final String MODID = "zcompression";
     static final String VERSION = "1.0";
-
-    @SidedProxy(clientSide = "proxy.ClientProxy", serverSide = "proxy.ServerProxy")
-    private static ServerProxy proxy;
-
     // Items
     public static ItemSlagiron slagiron = new ItemSlagiron();
-
-
     //Blocks, esp. for tconstruct
     public static BlockAardiumOre aardiumOre = new BlockAardiumOre();
     public static BlockAdamantiteOre adamantiteOre = new BlockAdamantiteOre();
@@ -70,7 +44,18 @@ public class ZCompression {
     public static BlockTiberiumOre tiberiumOre = new BlockTiberiumOre();
     public static BlockVibraniumre vibraniumOre = new BlockVibraniumre();
     public static BlockYrdeanOre yrdeanOre = new BlockYrdeanOre();
+    @SidedProxy(clientSide = "proxy.ClientProxy", serverSide = "proxy.ServerProxy")
+    private static ServerProxy proxy;
 
+    private static void registerBlock(Block block) {
+        GameRegistry.register(block);
+        GameRegistry.register(new ItemBlock(block).setRegistryName(block.getRegistryName()));
+    }
+
+    private static void registerBlock(BasicBlockOreGlow block) {
+        GameRegistry.register(block);
+        GameRegistry.register(new ItemBlock(block).setRegistryName(block.getRegistryName()));
+    }
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent e) {
@@ -109,16 +94,6 @@ public class ZCompression {
     @EventHandler
     public void postInit(FMLPostInitializationEvent e) {
 
-    }
-
-    private static void registerBlock(BasicBlockOre block) {
-        GameRegistry.register(block);
-        GameRegistry.register(new ItemBlock(block).setRegistryName(block.getRegistryName()));
-    }
-    
-    private static void registerBlock(BasicBlockOreGlow block) {
-        GameRegistry.register(block);
-        GameRegistry.register(new ItemBlock(block).setRegistryName(block.getRegistryName()));
     }
 
 
