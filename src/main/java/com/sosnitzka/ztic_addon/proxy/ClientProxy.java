d860ed16d1bb49ed9b23fa918cd67b578eecd256
Robert Sosnitzka <robert.sosnitzka@gmail.com>
2016-05-19 00:14:04 +0200

    Finally fixed textures and colors of blocks and tools for every of the 32 additional ores.

--- src/main/java/com/sosnitzka/ztic_addon/proxy/ClientProxy.java
+++ src/main/java/com/sosnitzka/ztic_addon/proxy/ClientProxy.java
@@ -5,14 +5,16 @@ import com.sosnitzka.ztic_addon.Items;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.item.Item;
 import net.minecraftforge.fluids.Fluid;
 import slimeknights.tconstruct.library.client.MaterialRenderInfo;
+import slimeknights.tconstruct.library.client.texture.MetalTextureTexture;
 import slimeknights.tconstruct.library.materials.Material;
 
 import java.lang.reflect.Field;
 
-import static com.sosnitzka.ztic_addon.Materials.bismuth;
+import static com.sosnitzka.ztic_addon.Materials.*;
 
 public class ClientProxy extends ServerProxy {
 
@@ -55,11 +57,28 @@ public class ClientProxy extends ServerProxy {
         }
     }
 
-    public void setRenderInfo(Material material, Fluid fluid) {
-        if (material != bismuth) {
-            material.setRenderInfo(new MaterialRenderInfo.Metal(fluid.getColor(), 0.8f, 0.2f, 0f));
-        } else bismuth.setRenderInfo(new MaterialRenderInfo.BlockTexture("ztic_addon:blocks/bismuth_block"));
+    public void setRenderInfo(final Material material, Fluid fluid) {
+        /* if (material != bismuth) {
+            material.setRenderInfo(new MaterialRenderInfo.Metal(fluid.getColor(), 0.4f, 0.2f, 0f));
+        }  else bismuth.setRenderInfo(new MaterialRenderInfo.BlockTexture("ztic_addon:blocks/bismuth_block")); */
 
+        if (material == adamantite) renderTo(material, 0xd55cdf, 0.5f, 0.8f, 0.2f);
+        else if (material == solarium) renderTo(material, 0xffff00, 0.6f, 1.8f, 0.0f);
+        else if (material == proxideum) renderTo(material, 0x2d8f8b, 0.4f, 0.3f, 0.1f);
+        else if (material == tiberium) renderTo(material, 0x33ff33, 0.5f, 0.2f, 0.1f);
+        else if (material == bismuth) renderTo(material, 0xffffff, 0.4f, 0.2f, 0.0f);
+        else if (material == dyonite) renderTo(material, 0xffe240, 0.6f, 0.8f, 0.2f);
+        else {
+            material.setRenderInfo(new MaterialRenderInfo.BlockTexture("ztic_addon:blocks/" + material.getFluid().getName() + "_block"));
+        }
+    }
 
+    private void renderTo(Material material, final int f, final float shine, final float brightness, final float hueshift) {
+        material.setRenderInfo(new MaterialRenderInfo.AbstractMaterialRenderInfo() {
+            @Override
+            public TextureAtlasSprite getTexture(TextureAtlasSprite baseTexture, String location) {
+                return new MetalTextureTexture("ztic_addon:blocks/materials/adamantite", baseTexture, location, f, shine, brightness, hueshift);
+            }
+        });
     }
 }
