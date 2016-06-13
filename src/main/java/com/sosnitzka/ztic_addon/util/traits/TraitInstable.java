ec98820782ec360053dff31fe922fe249f08441c
Robert Sosnitzka <robert.sosnitzka@gmail.com>
2016-06-13 20:18:41 +0200

    Fixing Explosions in TraitInstable

--- src/main/java/com/sosnitzka/ztic_addon/util/traits/TraitInstable.java
+++ src/main/java/com/sosnitzka/ztic_addon/util/traits/TraitInstable.java
@@ -1,8 +1,6 @@
 package com.sosnitzka.ztic_addon.util.traits;
 
-import com.sosnitzka.ztic_addon.util.ZExplosion;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
@@ -11,6 +9,7 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import slimeknights.tconstruct.library.traits.AbstractTrait;
+import slimeknights.tconstruct.library.utils.ToolHelper;
 
 /**
  * Created by Robert on 14.05.2016.
@@ -26,29 +25,30 @@ public class TraitInstable extends AbstractTrait {
     @Override
     public void afterBlockBreak(ItemStack tool, World world, IBlockState state, BlockPos pos, EntityLivingBase player, boolean wasEffective) {
         if (MathHelper.getRandomIntegerInRange(random, 0, 100) > 2) {
-            if (!world.isRemote)
-
-                newZExplosion(player, pos.getX(), pos.getY(), pos.getZ(), 2f, true, true);
-
+            if (!world.isRemote) {
+                if (random.nextBoolean()) {
+                    Explode(player, pos.getX(), pos.getY(), pos.getZ());
+                } else Explode(null, pos.getX(), pos.getY(), pos.getZ());
+            }
+            ToolHelper.damageTool(tool, 11 + random.nextInt(10), null);
         }
     }
 
     @Override
     public void afterHit(ItemStack tool, EntityLivingBase player, EntityLivingBase target, float damage, boolean wasCritical, boolean wasHit) {
         BlockPos pos = target.getPosition();
-        if (MathHelper.getRandomIntegerInRange(random, 0, 100) < 2) {
-            newZExplosion(target, pos.getX(), pos.getY(), pos.getZ(), 2f, true, true);
+        if (MathHelper.getRandomIntegerInRange(random, 0, 100) > 2) {
+            if (!player.getEntityWorld().isRemote) {
+                if (random.nextBoolean()) {
+                    Explode(player, pos.getX(), pos.getY(), pos.getZ());
+                } else Explode(target, pos.getX(), pos.getY(), pos.getZ());
+            }
+            ToolHelper.damageTool(tool, 3 + random.nextInt(18), null);
         }
     }
 
-    private ZExplosion newZExplosion(Entity entityIn, double x, double y, double z, float strength, boolean isFlaming, boolean isSmoking) {
-        ZExplosion explosion = new ZExplosion(entityIn.worldObj, entityIn, x, y, z, strength, isFlaming, isSmoking);
-        if (net.minecraftforge.event.ForgeEventFactory.onExplosionStart(entityIn.worldObj, explosion)) return explosion;
-        explosion.doExplosionA();
-        explosion.doExplosionB(true);
-        return explosion;
+    private void Explode(EntityLivingBase e, double x, double y, double z) {
+        e.getEntityWorld().newExplosion(e, x, y, z, 1.2f + random.nextFloat() * 5, random.nextBoolean(), true);
     }
-
-
 
 }
