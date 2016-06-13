a7d937f75839bbd5ecec05670d5528fa0faa24bf
Giovanni Harting <539@idlegandalf.com>
2016-06-13 22:35:44 +0200

    added some stuff

--- src/main/java/com/sosnitzka/ztic_addon/generic/BasicBlock.java
+++ src/main/java/com/sosnitzka/ztic_addon/generic/BasicBlock.java
@@ -2,9 +2,15 @@ package com.sosnitzka.ztic_addon.generic;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 public class BasicBlock extends Block {
+    public static final PropertyBool ISSUN = PropertyBool.create("issun");
     private String oreDictPrefix;
+    private int lVal;
 
     public BasicBlock(String name, Material material, float hardness, float resistance, int harvest, float lightLevel, String oreDictPrefix) {
         super(material);
@@ -16,7 +22,11 @@ public class BasicBlock extends Block {
         setLightLevel(lightLevel);
         this.oreDictPrefix = oreDictPrefix;
 
+    }
 
+    public BasicBlock(String name, Material material, float hardness, float resistance, int harvest, float lightLevel, String oreDictPrefix, int activatedLL) {
+        this(name, material, hardness, resistance, harvest, lightLevel, oreDictPrefix);
+        this.lVal = activatedLL;
     }
 
     public BasicBlock(String name, Material material, float hardness, float resistance, int harvest) {
@@ -40,4 +50,12 @@ public class BasicBlock extends Block {
     }
 
 
+    @Override
+    public int getLightValue(IBlockState state, IBlockAccess world, BlockPos pos) {
+        if (state.getValue(ISSUN)) {
+            return lVal;
+        } else {
+            return this.lightValue;
+        }
+    }
 }
