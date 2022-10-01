43670a40fb0b8106bad516fa65d0d51e242fac03
huige <61108540+huige233@users.noreply.github.com>
2022-10-01 23:40:18 +0800

    fix a bug

--- src/main/java/com/sosnitzka/taiga/traits/TraitSlaughtering.java
+++ src/main/java/com/sosnitzka/taiga/traits/TraitSlaughtering.java
@@ -2,7 +2,9 @@ package com.sosnitzka.taiga.traits;
 
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.monster.EntitySlime;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
@@ -25,13 +27,18 @@ public class TraitSlaughtering extends AbstractTrait {
         World w = event.getEntity().getEntityWorld();
         if (event.getSource().getTrueSource() instanceof EntityPlayer) {
             EntityPlayer player = (EntityPlayer) event.getSource().getTrueSource();
-            if (!w.isRemote && event.getEntity() instanceof EntityLiving && !(event.getEntity() instanceof
-                    EntityPlayer) && TinkerUtil.hasTrait(TagUtil.getTagSafe(player.getHeldItemMainhand()),
-                    identifier)) {
-                ItemStack i = event.getDrops().get(random.nextInt(event.getDrops().size())).getItem();
-                event.getDrops().add(new EntityItem(w, event.getEntity().posX, event.getEntity().posY, event
-                        .getEntity().posZ, new ItemStack(i.getItem(), random.nextInt(4) + 1, i.getMetadata(), i
-                        .getTagCompound())));
+            if (!w.isRemote && event.getEntity() instanceof EntityLiving && !(event.getEntity() instanceof EntityPlayer) && TinkerUtil.hasTrait(TagUtil.getTagSafe(player.getHeldItemMainhand()), identifier)) {
+                if (event.getEntity() instanceof EntitySlime) {
+                    if (((EntitySlime) event.getEntity()).isSmallSlime()) {
+                        ItemStack s = new ItemStack(Items.SLIME_BALL,2);
+                        event.getDrops().add(new EntityItem(w, event.getEntity().posX, event.getEntity().posY, event.getEntity().posZ, new ItemStack(s.getItem(), random.nextInt(4) + 1, s.getMetadata(), s.getTagCompound())));
+                    }
+                } else {
+                    if (event.getDrops().size() != 0) {
+                        ItemStack i = event.getDrops().get(random.nextInt(event.getDrops().size())).getItem();
+                        event.getDrops().add(new EntityItem(w, event.getEntity().posX, event.getEntity().posY, event.getEntity().posZ, new ItemStack(i.getItem(), random.nextInt(4) + 1, i.getMetadata(), i.getTagCompound())));
+                    }
+                }
             }
         }
     }
